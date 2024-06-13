a31ef19d151e1344f4fbb0c4f3cc01ded5ca5b5a
赵政凯 <18204494862@163.com>
2024-06-13 10:53:26 +0800

    提交代码

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="box">
+    <h1>App根组件</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.box {
+  width: 600px;
+  height: 400px;
+  background-color: black;
+  h1 {
+    color: white;
+  }
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import { createApp } from "vue";
+import App from "./App.vue";
+
+createApp(App).mount("#app");
